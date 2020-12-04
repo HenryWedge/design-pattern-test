b46a41617e219f54a992081b513c8b2a615045d6
Hendrik Reiter <here@ppi.de>
2020-12-04 09:43:44 +0100

    remove unused main

--- src/main/java/de/ppi/here/tcu/Main.java
+++ src/main/java/de/ppi/here/tcu/Main.java
@@ -2,15 +2,10 @@ package de.ppi.here.tcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import de.ppi.here.tcu.entity.Bank;
-import de.ppi.here.tcu.composite.precondition.PreconditionNotFulfilledException;
-import de.ppi.here.tcu.result.DuplicateEntityException;
-import de.ppi.here.tcu.adminservice.inherit.BankAdministrationService;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        //SpringApplication.run(Main.class, args);
-        System.out.println("mand42".toUpperCase());
+        SpringApplication.run(Main.class, args);
     }
 }
